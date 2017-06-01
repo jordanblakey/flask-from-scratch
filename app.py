935f93bdb5bb232f2da5aba942cfda5c3a9d6b40
Jordan Blakey <jordan@jordanblakey.com>
2017-06-01 08:29:25 -0500

    Quick commit.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
@@ -8,23 +8,26 @@ app = Flask(__name__)
 
 Articles = Articles()
 
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+  return render_template('home.html')
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+  return render_template('about.html')
+
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles = Articles)
+  return render_template('articles.html', articles=Articles)
+
 
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id = id)
+  return render_template('article.html', id=id)
 
-# @app.route('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
