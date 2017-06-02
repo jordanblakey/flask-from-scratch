556056898dd662dc7cca63c89a05b33a0cd31024
Jordan Blakey <jordan@jordanblakey.com>
2017-06-02 00:01:26 -0500

    Quick commit.

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flaskfromscratch'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# Initialize MySQL
+mysql = MySQL(app)
+
 Articles = Articles()
 
 
@@ -29,5 +38,45 @@ def article(id):
   return render_template('article.html', id=id)
 
 
+class RegisterForm(Form):
+  name = StringField('Name', [validators.Length(min=1, max=50)])
+  username = StringField('Username', [validators.Length(min=4, max=25)])
+  email = StringField('Email', [validators.Length(min=6, max=50)])
+  password = PasswordField('Password', [
+      validators.DataRequired(),
+      validators.EqualTo('confirm', message='Passwords do not match')
+  ])
+  confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  form = RegisterForm(request.form)
+  if request.method == 'POST' and form.validate():
+    name = form.name.data
+    email = form.email.data
+    username = form.username.data
+    password = sha256_crypt.encrypt(str(form.password.data))
+
+    # Create cursor
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users(name, email, username, password) VALUES (%s, %s, %s, %s)",
+                (name, email, username, password))
+
+    # Commit to DB
+    mysql.connection.commit()
+
+    # Close connection
+    cur.close()
+
+    flash('You are now registered and can log in.', 'success')
+
+    return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+  return render_template('register.html', form=form)
+
+
 if __name__ == '__main__':
+  app.secret_key = 'secret123'
   app.run(debug=True)
